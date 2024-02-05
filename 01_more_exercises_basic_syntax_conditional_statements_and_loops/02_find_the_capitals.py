85f8ac13328d56fde04ed468a02aaaa466279d7a
StefanHaralambov <148768646+StefanHaralambov@users.noreply.github.com>
2024-02-05 15:48:55 +0200

    Same method but exploded

--- 01_more_exercises_basic_syntax_conditional_statements_and_loops/02_find_the_capitals.py
+++ 01_more_exercises_basic_syntax_conditional_statements_and_loops/02_find_the_capitals.py
@@ -1,5 +1,13 @@
 word = input()
 
-foo = ([pos for pos, char in enumerate(word) if char.isupper()])
+# foo = ([pos for pos, char in enumerate(word) if char.isupper()])
 
-print(foo)
+# print(foo)
+
+pos = []
+
+for letter, char in enumerate(word):
+    if char.isupper():
+        pos.append(letter)
+
+print(pos)
