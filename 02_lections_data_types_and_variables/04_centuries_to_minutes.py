a117d4be0ce94958f27c8ea8242e6e8cb7492ad8
StefanHaralambov <148768646+StefanHaralambov@users.noreply.github.com>
2024-02-11 09:29:05 +0200

    Update 04_centuries_to_minutes.py

--- 02_lections_data_types_and_variables/04_centuries_to_minutes.py
+++ 02_lections_data_types_and_variables/04_centuries_to_minutes.py
@@ -1,10 +1,8 @@
 unit = int(input())
 
-# ⦁	Assume that one year has 365.2422 days on average
-
 years = unit * 100
-days = round(years * 365.2422)
+days = years * 365.2422
 hours = days * 24
 minutes = hours * 60
 
-print(f"{unit} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
+print(f"{unit} centuries = {years:.0f} years = {days:.0f} days = {hours:.0f} hours = {minutes:.0f} minutes")
