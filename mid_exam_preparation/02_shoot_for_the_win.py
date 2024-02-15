ac316f683ed35c34ce4571551bde9dc733977925
StefanHaralambov <148768646+StefanHaralambov@users.noreply.github.com>
2024-02-16 00:58:09 +0200

    .

--- mid_exam_preparation/02_shoot_for_the_win.py
+++ mid_exam_preparation/02_shoot_for_the_win.py
@@ -1,19 +1,28 @@
-targets = input().split()
+targets = [int(target) for target in input().split()]
+
+target_down = 0
 
 command = input()
 
 while command != "End":
-    command = int(command)
-    if int(command) in range(len(targets)):
-        substraction = 0
-        print(substraction)
-        targets[command] = "-1"
-        print(targets)
-    else:
-        print("False")
+    target_index = int(command)
+
+    if target_index in range(len(targets)):
+        for index in range(len(targets)):
+            if targets[index] == -1:
+                continue
+            if target_index == index:
+                continue
+            if targets[target_index] < targets[index]:
+                targets[index] -= targets[target_index]
+
+            else:
+                targets[index] += targets[target_index]
+        targets[target_index] = -1
+        target_down += 1
 
     command = input()
 
+result = " ".join(map(str, targets))
 
-
-
+print(f"Shot targets: {target_down} -> {result}")
--- mid_exam_preparation/test.py
+++ mid_exam_preparation/test.py
@@ -1,3 +1,9 @@
-thislist = ["apple", "banana", "cherry"]
-thislist[1] = "blackcurrant"
-print(thislist)
+list1 = ["10", "20", "30", "40"]
+int_lst = [int(x) for x in list1]
+print(int_lst)
+integer = int(input())
+
+for i in range(len(int_lst)):
+    if int_lst[i] > integer:
+        int_lst[i] += integer
+        print(i)
