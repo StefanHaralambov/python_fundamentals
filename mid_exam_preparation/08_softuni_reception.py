7d7e7450c0514a54595a3f6f5f0a96c5818163cf
StefanHaralambov <148768646+StefanHaralambov@users.noreply.github.com>
2024-02-18 17:15:24 +0200

    Update 08_softuni_reception.py

--- mid_exam_preparation/08_softuni_reception.py
+++ mid_exam_preparation/08_softuni_reception.py
@@ -10,24 +10,6 @@ employee_efficiency = employee_1 + employee_2 + employee_3
 
 hours_count = math.ceil(students_count / employee_efficiency)
 
-employee_breaks = math.floor(students_count / employee_efficiency / 3)
-
-hours_count_final = employee_breaks + hours_count
-
-print(f"Time needed: {hours_count_final}h.")
-
-import math
-
-employee_1 = int(input())
-employee_2 = int(input())
-employee_3 = int(input())
-
-students_count = int(input())
-
-employee_efficiency = employee_1 + employee_2 + employee_3
-
-hours_count = math.ceil(students_count / employee_efficiency)
-
 if hours_count % 3 != 0:
     employee_breaks = math.floor(hours_count / 3)
 else:
