13770cf2dc5b3cd26a04882c587c9e0cdcc34ec3
StefanHaralambov <148768646+StefanHaralambov@users.noreply.github.com>
2024-02-21 10:52:12 +0200

    Update test.py

--- mid_exam_preparation/test.py
+++ mid_exam_preparation/test.py
@@ -1,9 +1,1 @@
-list1 = ["10", "20", "30", "40"]
-int_lst = [int(x) for x in list1]
-print(int_lst)
-integer = int(input())
-
-for i in range(len(int_lst)):
-    if int_lst[i] > integer:
-        int_lst[i] += integer
-        print(i)
+lst = [10, 20 , 30, 40]
